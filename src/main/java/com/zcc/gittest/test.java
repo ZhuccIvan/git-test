7ea7f18478c38128cafb63f796a007f61067ec7e
zcc <1095582370@qq.com>
2022-06-01 07:40:08 +0800

    version 1.4

--- src/main/java/com/zcc/gittest/test.java
+++ src/main/java/com/zcc/gittest/test.java
@@ -5,6 +5,6 @@ public class test {
         System.out.println("git-test 1.0");
         System.out.println("git-test 1.1");
         System.out.println("git-test 1.2");
-        System.out.println("git-test hot-fix change");
+        System.out.println("git-test master change");
     }
 }
