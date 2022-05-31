5af7558e6be6291bd378a0754b7bbcbd7178ad9b
zcc <1095582370@qq.com>
2022-06-01 07:27:51 +0800

    version 1.1

--- src/main/java/com/zcc/gittest/test.java
+++ src/main/java/com/zcc/gittest/test.java
@@ -3,5 +3,6 @@ package com.zcc.gittest;
 public class test {
     public static void main(String[] args) {
         System.out.println("git-test 1.0");
+        System.out.println("git-test 1.1");
     }
 }
